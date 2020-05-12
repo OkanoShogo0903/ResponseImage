dfe56f00cd46107075b1a9b11e78504f46e6bc09
okano_thinkpad2 <okanosyogo@gmail.com>
2020-05-12 18:02:06 +0900

    [NEW] フォームを追加した。しかし新しく属性を追加するためのUIが決まらない。

--- src/common/Api.ts
+++ src/common/Api.ts
@@ -2,12 +2,13 @@ import Vue from 'vue';
 
 import axios from 'axios';
 
-var base_endpoint:  string = 'https://img-database.herokuapp.com/'
-//private base_endpoint:  string = 'http://localhost:8080/'
+//var base_endpoint:  string = 'https://img-database.herokuapp.com/'
+var base_endpoint:  string = 'http://localhost:8080/'
 
 var get_all_image:  string = 'all_image'
 var get_image:      string = 'images'
 var get_all_genre:  string = 'all_genre'
+var get_all_charactor: string = 'all_charactor'
 var put_registe:    string = 'registe'
 
 export function getAllImageUrl() {
@@ -61,4 +62,28 @@ export function getAllGenre() {
       console.log(error);
   })
   return genres
+}
+
+export function getAllCharactor() {
+  var characters = new Array()
+  axios.get(base_endpoint + get_all_charactor)
+  .then(res => {
+    if (res.status === 200) {
+      for (var i in res.data) {
+        characters.push( res.data[i]['character_name'] )
+      }
+    }
+  })
+  .catch(error => {
+    console.log(error);
+  })
+  return characters;
+}
+
+export function pressUploadUrl(a: (string|null), b: (string|null)[]) {
+    return base_endpoint + put_registe + 
+      '?character=' + a + 
+      '&primary='   + b[0] + 
+      '&secondary=' + b[1] + 
+      '&tertiary='  + b[2];
 }
--- src/components/UploadPlace.vue
+++ src/components/UploadPlace.vue
@@ -1,14 +1,59 @@
 <template>
-  <div>
-    <input type="text" v-model="value">
-    <vue-dropzone v-if="dropzoneOptions" ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
-  </div>
+  <v-container>
+    <v-form ref="form" v-model="valid" lazy-validation>
+      <v-select
+        v-model="select_charactor"
+        :items="candidate_charactor"
+        :rules="[v => !!v || 'Item is required']"
+        label="キャラクターを指定してください"
+      ></v-select>
+
+      <v-text-field
+        v-model="new_charactor"
+        :rules= "[
+          v => !!v || '追加するキャラクター名を入力してください',
+          v => (v && v.length <= 15) || 'キャラクター名は15文字以下にしてください'
+        ]"
+        :counter="15"
+        label="新しく追加するキャラクター名"
+      ></v-text-field>
+
+      <v-select
+        v-model="select_genre"
+        :items="candidate_genre"
+        :rules="[v => !!v || 'Item is required']"
+        label="ジャンルを指定してください"
+        required
+      ></v-select>
+
+      <v-checkbox
+        v-model="checkbox"
+        :rules="[v => !!v || 'You must check to continue!']"
+        label="R18コンテンツではありません"
+        required
+      ></v-checkbox>
+
+      <vue-dropzone v-if="dropzoneOptions" ref="myVueDropzone" id="dropzone" :options="dropzoneOptions"></vue-dropzone>
+
+      <v-btn
+        :disabled="!valid"
+        @click="submit"
+        class=submit_btn
+      >
+        画像を投稿
+      </v-btn>
+
+    </v-form>
+
+  </v-container>
 </template>
 
 <script lang="ts">
-  import vue2Dropzone from 'vue2-dropzone'
-  import 'vue2-dropzone/dist/vue2Dropzone.min.css'
-  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import vue2Dropzone from 'vue2-dropzone';
+  import 'vue2-dropzone/dist/vue2Dropzone.min.css';
+  import { Component, Vue } from 'vue-property-decorator';
+
+  import { getAllGenre, getAllCharactor, pressUploadUrl } from '@/common/Api';
 
   @Component({
     components: {
@@ -19,23 +64,55 @@
   export default class Navigation extends Vue {
     // TODO: アップロードゾーンとアップロードボタンを別に用意する
     // TODO: キャラクター一覧のドロップダウンから選択できるようにする
-    @Prop()
-    public upload_endpoint!: string;
 
-    public character: ( string | null ) = null
-    public attributes: ( string | null )[] = ["あいさつ", null, null] 
+    public valid: ( Boolean | null ) = true;
+    public checkbox: Boolean = false;
 
-    public upload_url: string = this.upload_endpoint + '?character=' + this.character + '&primary=' + this.attributes[0] + '&secondary=' + this.attributes[1] + '&tertiary=' + this.attributes[2]
+    private new_charactor: ( String | null ) = null;
+
+    public none_phrase: string = '選択しない';
+    public add_phrase: string = '新しく追加する';
+
+    public candidate_charactor: string[] = Array();
+    public candidate_genre: string[] = Array();
+
+    public select_charactor: ( string | null ) = null;
+    public select_genre: ( string | null )[] = ["あいさつ", null, null];
 
     public dropzoneOptions: any = {
-      url: this.upload_url,
+      url: pressUploadUrl(this.select_charactor, this.select_genre),
       method: 'put',
       maxFilesize: 5,
-    }
-    public created(){
-      console.log(this.upload_endpoint)
-      console.log(this.upload_url)
-    }
+    };
+
+    private submit () {
+      console.log("pressed submit")
+        /*
+        if ( this.$refs.form.validate() ) {
+          axios.post('/api/submit', {
+            charactor: this.select_charactor,
+            genre_primary: this.select_genre,
+            ....
+          })
+        }
+        */
+    };
+
+    public created () {
+      console.log( this.add_phrase )
+      this.candidate_charactor = getAllCharactor();
+      this.candidate_charactor.unshift(this.add_phrase)
+      this.candidate_charactor.push(this.none_phrase)
+
+      this.candidate_genre = getAllGenre();
+      this.candidate_genre.push(this.add_phrase)
+    };
   }
 </script>
 
+<style scoped>
+.submit_btn {
+  margin: 30px;
+  width:20%; height:100%;
+}
+</style>
